c0b90471a1e51d505c9afcbe27db4437e3a79384
Cyrrian <william.r.stewart@gmail.com>
2025-06-15 17:36:54 -0500

    added constants.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS,ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+
 #******************************
 # Function: main
 #******************************
 def main():
 
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
