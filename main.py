04eccbb4371a72369b687f76b02c5b5f063e7ec6
Cyrrian <william.r.stewart@gmail.com>
2025-06-15 19:18:58 -0500

    added player

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS,ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+from player import Player
 
 #******************************
 # Function: main
 #******************************
 def main():
     pygame.init()
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
