3f3b628e76d63d886af53d14fe0d879b00b16222
Cyrrian <william.r.stewart@gmail.com>
2025-06-15 19:06:03 -0500

    updated game loop to limit fps and added circleshape.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 # Function: main
 #******************************
 def main():
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        screen.fill((0,0,0))
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+        
+        screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
